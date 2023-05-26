59179130d81c0d1d13d4560883972f862c13e267
Nikolay Grigoritev <gnarus@inbox.ru>
2023-05-26 17:12:32 +0300

    5 commit (тестирую GUI)

--- controller.py
+++ controller.py
@@ -3,7 +3,7 @@ def start():
     viev.privet() # Приветствие
     while True:
         viev.menu()
-        point = input('введите команду из меню: ')
+        point = input('Введите команду из меню: ')
         # 1 - показать контакты
         if point == '1':
             model.get_data()
@@ -11,8 +11,8 @@ def start():
 
         # 2 - добавить контакты
         elif point == '2':
-            contact = input('введите имя и фамилию котакта: ')
-            contact_num = input('введите номер телефона контакта: ')
+            contact = input('Введите имя и фамилию нового котакта: ')
+            contact_num = input('Введите номер телефона контакта: ')
             res = model.add_contact(contact, contact_num)
             if res:
                 viev.success(res)
@@ -21,15 +21,15 @@ def start():
 
         # 3 - поиск номера телефона
         elif point == '3':
-            surname = input('введите фамилию для поиска номера: ')
+            surname = input('Введите фамилию для поиска номера: ')
             resalt = model.find(surname)
             # viev.show_find(resalt)
             input() # ожидание
 
         # 4 - изменить номер телефона
         elif point == '4':
-            surname1 = input('введите фамилию для изменения номера:')
-            number = input('введите новый номер')
+            surname1 = input('Введите фамилию для изменения номера:')
+            number = input('Введите новый номер')
             res1 = model.add_number(surname1,number) 
             if res1:
                 viev.success_number(res1) 
@@ -37,7 +37,7 @@ def start():
                 viev.not_number(res1)
         # 5 - удалить контакт
         elif point == '5':
-            surname2 = input('введите фамилию для удаления контакта:')
+            surname2 = input('Введите фамилию для удаления контакта:')
             res2 = model.del_contact(surname2) 
             if res2:
                 viev.success_del(res2) 
